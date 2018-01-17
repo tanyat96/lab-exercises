abe5c4796d77066061d7dfbff90e1f6a7b71739e
Tanya Takahashi <tanyat96@gmail.com>
2018-01-16 21:16:44 -0800

    Try 1

--- exercise-wk2/exercise1.R
+++ exercise-wk2/exercise1.R
@@ -5,11 +5,12 @@ initials <- "?. ?."
 
 my.vector <- c(my.num, initials)
 
+
 # run ?sum to get more info
 vector.sum <- sum(my.vector)
 
 # Describe why this doesn't work: 
-# 
+# The "?, ?" is not numerical.
 
 install.packages("stringr")
 
@@ -18,23 +19,32 @@ my.line <- "Hey, hey, this is the library"
 print(str_length(my.line))
 
 # Describe why this doesn't work: 
-# 
+# I did not load the stringr package first.
 
 said.the.famous <- paste(my.line, " - ", initial)
 
 # Describe why this doesn't work: 
-# 
-
+# There is no object "initial," only "initials."
 
 ## Part 2 - Vector and function practice
 
 # Make a vector and use typeof to check what type R considers it to be
+v1 <- c("Iris, John, Tanya")
+typeof(v1) 
+typeof(c(8, 9, 10))
+typeof(c(8, "lucky"))
 
 # Write a function `CompareLength` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
+CompareLength <- function(v2, v3) {
+  diff <- (abs(length(v2) - length(v3)))
+  return(paste("The difference in lengths is", diff))
+}
+# Step 1: Find out how long each of the vectors are
+# Step 2: Subtract vector lengths from each other
+# Step 3: Return the sentence "The difference in lengths is N"
 
-
-# Pass two vectors of different length to your `CompareLength` function
+C# Pass two vectors of different length to your `CompareLength` function
 
 
 # Write a function `DescribeDifference` that will return one of the following statements:
